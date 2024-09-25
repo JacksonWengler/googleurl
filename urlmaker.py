e2eb19e5d946609e28f6f617cbd7887bd098797c
JacksonWengler <jwengler4304@stu.neisd.net>
2024-09-24 17:35:17 -0700

    This is to resolve debugging pull request

--- urlmaker.py
+++ urlmaker.py
@@ -5,5 +5,5 @@ search= input('What do you want to search for?')
 #turn spaces into google spaces %20
 search= search.replace(' ', '%20')
 google=('google.com/search?q=')
-print(google+search)
+#print(google+search)
 url=search
